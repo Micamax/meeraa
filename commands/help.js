1226b7cfdc5dcd40790d11cb7007dc60ea0d8368
Micamax <33392428+Micamax@users.noreply.github.com>
2018-03-23 00:11:10 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send('**NSFW Commands \"-nsfw\":** \n `4k` \n `amateur`\n `asian`\n `ass`\n `boobs`\n `cosplay`\n `gif`\n `hentai`\n `fuck`\n `pornhub`\n `pussy`\n `snapchat`\n `milf`\n `bbw`\n `dick`\n `public`\n `uniform`\n\n**Other Commands:** \n `help`\n `invite` \n `reload` \n `up`')
+}
